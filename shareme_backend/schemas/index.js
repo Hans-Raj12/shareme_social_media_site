6dde792e6f476c749e7f8c006eb4b9790bd4d5b2
Hans Raj <75563900+Hans-Raj12@users.noreply.github.com>
2023-05-29 20:56:56 +0500

    Files not uploading on github, bug fixed

---
+++ shareme_backend/schemas/index.js
@@ -0,0 +1,6 @@
+import user from './user'
+import save from './save'
+import pin from './pin'
+import postedBy from './postedBy'
+import comment from './comment'
+export const schemaTypes = [user,save,pin,postedBy,comment]
